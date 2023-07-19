433203234ef7f89185d9e4d987b88abcadbefafa
msm031 <msm031@naver.com>
2023-07-19 11:34:34 +0900

    Initial commit

---
+++ app/src/main/java/com/zweimonde/myfirstandroid_genie/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.zweimonde.myfirstandroid_genie
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.zweimonde.myfirstandroid_genie.ui.theme.MyFirstAndroid_GenieTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyFirstAndroid_GenieTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyFirstAndroid_GenieTheme {
+        Greeting("Android")
+    }
+}
